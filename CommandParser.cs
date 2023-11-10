e978a6449ffc1b031f8411f1587031e77e936802
archie2808 <a.gledhill.8333@student.leedsbeckett.ac.uk>
2023-11-10 20:04:04 +0000

    Command Parser now reads and executes one line at a time. Furthermore, implemented move to command to position the pen at different points on the canvas. also added XML documentation.

--- CommandParser.cs
+++ CommandParser.cs
@@ -9,18 +9,38 @@ using System.Drawing;
 namespace WindowsFormsApp1
 {
     /// <summary>
-    /// class for parsing commands
+    /// The <c>CommandParser</c> class is responsible for interpreting and executing user commands. 
     /// </summary>
-    class CommandParser
+    class CommandParser 
     {
-        public CommandParser()
-        {
+        private Point penPosition;
+        private Bitmap drawingSurface;
+        private readonly TextBox outputTextBox;
 
-        }
         /// <summary>
-        /// add parameters for null command, implement case for a draw command
+        /// Initializes a new instance of the <c>CommandParser</c> class.
+        /// </summary>
+        /// <param name="output">The textbox control where output messages are displayed</param>
+        /// <param name="surface">The bitmap surface on which drawing commands are executed</param>
+        /// <remarks>
+        /// The constructor sets up the initial state of the Parser, including setting the initial pen position
+        /// and associating the output TextBox and drawing Bitmap
+        /// </remarks>
+        public CommandParser(TextBox output, Bitmap surface)
+        {
+            penPosition = new Point(0, 0);
+            outputTextBox = output;
+            drawingSurface = surface; 
+        }
+
+        /// <summary>
+        /// Executes user commands based on provided inputs
         /// </summary>      
-        /// <param name="command"></param>
+        /// <param name="command">The command string to be parsed and executed</param>
+        /// <remarks>
+        /// This method processes the command string, identifies the type of command. (e.g. moveto), 
+        /// and executes the corresponding action. It also handles invalid commands.
+        /// </remarks>
         public void ExecuteCommand(string command)
         {
             command = command.Trim();
@@ -36,14 +56,53 @@ namespace WindowsFormsApp1
 
             switch (action)
             {
-                case "draw":
-                    DrawCommand(commandParts);
+                case "moveto":
+                    MoveToCommand(commandParts);
                     break;
+                    
 
                 default:
-                    MessageBox.Show($"unknown command: {action}"); 
+                    outputTextBox.AppendText($"Unknown command: {action}\n"); 
                     break;
             }
+            
+        }
+        
+        /// <summary>
+        /// Processes the 'moveto' command, updating the pen position and drawing on the Bitmap
+        /// </summary>
+        /// <param name="commandParts">The parameters of the commanf, such as the co-oridinates</param>
+        /// <remarks>
+        /// This method updates the pen position based on the coordinates provided in the command.
+        /// it then calls on <c>DrawOnBitmap</c> to reflect this change on the drwaing surface.
+        /// </remarks>
+        private void MoveToCommand(string[] commandParts)
+        {
+            if (commandParts.Length == 3 && int.TryParse(commandParts[1], out int x) && int.TryParse(commandParts[2], out int y)) 
+            {
+                penPosition = new Point(x, y);
+                DrawOnBitmap();
+                outputTextBox.AppendText($"Pen moved to ({x}, {y}). \n");
+            }
+            else
+            {
+                outputTextBox.AppendText("invalid move to command. \n");
+            }
+        }
+        /// <summary>
+        /// Draws on bitmaps surface at the current pen position
+        /// </summary>
+        /// <remarks>
+        /// This method is called after updating the pen position to visually represent the pens's 
+        /// new location on the bitmap. It currently draws a small red circle at the pen position.
+        /// </remarks>
+        private void DrawOnBitmap()
+        {
+            using (Graphics g = Graphics.FromImage(drawingSurface))
+            {
+                g.FillEllipse(Brushes.Red, penPosition.X - 2, penPosition.Y - 2, 4, 4);
+            }
+            
         }
       
 
--- Form1.cs
+++ Form1.cs
@@ -10,23 +10,47 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
+    /// <summary>
+    /// The main form of the application, responsible for handling user interactions and displaying the graphical interface.
+    /// </summary>
     public partial class Form1 : Form
     {
         private CommandParser commandParser;
+        private Bitmap drawingSurface;
+
+        /// <summary>
+        /// Initialized a new instance of the <c>Form1</c> class
+        /// </summary>
+        /// <remarks>
+        /// This constructor intializes the componenents of the form, sets up the drawing surface,
+        /// and creates an instance of the Command Parser class
+        /// </remarks>
         public Form1()
         {
             InitializeComponent();
-            commandParser = new CommandParser();
+            drawingSurface = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = drawingSurface;
+            commandParser = new CommandParser(textBox2, drawingSurface);
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// Handles the click event of the 'run' button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <remarks>
+        /// This method retrieves the command from textBox1, executes it using the command parser, and then reinitialises 
+        /// the command parser with the updated drawing surface
+        /// </remarks>
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            string command = textBox1.Text; // Get the command from textBox1
+            commandParser.ExecuteCommand(command); // Parses textBox2 for output
+            commandParser = new CommandParser(textBox2, drawingSurface);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -39,9 +63,28 @@ namespace WindowsFormsApp1
 
         }
         
+        /// <summary>
+        /// Handles the Paint event of the picture box control
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <remarks>
+        /// This method redraws the image on the drawing surface whenever the picture box is repainted. 
+        /// </remarks>
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-           
+            e.Graphics.DrawImage(drawingSurface, 0, 0);
+            pictureBox1.Refresh();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
